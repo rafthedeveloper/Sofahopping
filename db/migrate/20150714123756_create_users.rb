b7f2f408df8d70287de7baa3766c9c8a7e328a83
Rafael Maldonado <RafaelJMaldonado@gmail.com>
2015-07-14 11:18:59 -0400

    Adjust models to have all member info stored in usr model.

--- db/migrate/20150714123756_create_users.rb
+++ db/migrate/20150714123756_create_users.rb
@@ -4,6 +4,12 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
+      t.string :fname , null: false
+      t.string :lname , null: false
+      t.string :gender , null: false
+      t.date :birthday , null: false
+      t.string :location, null: false
+      t.string :hosting_status, null: false
 
       t.timestamps
     end
