557b5cb2d21fad62e3940ca379cb046227d28fb9
App Academy Student <appacademy@Apps-Mac-mini-17.local>
2015-07-14 09:07:15 -0400

    Setup backbone structure, add user login.

---
+++ app/assets/javascripts/sofa_hopping.js
@@ -0,0 +1,13 @@
+window.SofaHopping = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  SofaHopping.initialize();
+});
