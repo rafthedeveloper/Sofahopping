d462a5e75b46e74f6dc301fcf2fb1598c41ec74a
Rafael Maldonado <RafaelJMaldonado@gmail.com>
2015-07-15 14:50:51 -0400

    Update seed data and improve view of traveler search.

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -53,7 +53,7 @@ SofaHopping.Routers.Router = Backbone.Router.extend({
     hosts.fetch({ data: { status: "ACCEPTING GUESTS" }});
 
     var membersView = new SofaHopping.Views.MembersView({
-      collection: hosts, currentUser: this.currentUser })
+      collection: hosts, currentUser: this.currentUser, searchType: "hosts" })
     this._swapView(membersView);
 
   },
@@ -62,7 +62,8 @@ SofaHopping.Routers.Router = Backbone.Router.extend({
     var allMembers = new SofaHopping.Collections.Users();
     allMembers.fetch();
 
-    var membersView = new SofaHopping.Views.MembersView({ collection: allMembers, currentUser: this.currentUser })
+    var membersView = new SofaHopping.Views.MembersView({
+      collection: allMembers, currentUser: this.currentUser, searchType: "all" })
     this._swapView(membersView);
   },
 
@@ -70,7 +71,8 @@ SofaHopping.Routers.Router = Backbone.Router.extend({
     var allTravelers = new SofaHopping.Collections.Users();
     allTravelers.fetch({ data: { trips : true }});
 
-    var travelersView = new SofaHopping.Views.MembersView({ collection: allTravelers, currentUser: this.currentUser })
+    var travelersView = new SofaHopping.Views.MembersView({
+      collection: allTravelers, currentUser: this.currentUser, searchType: "travelers" })
     this._swapView(travelersView);
   },
 
