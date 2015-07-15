9b7a0434a6b20103f82c5a5dc8116d6bb42b3de9
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-07-15 11:12:14 -0400

    Add #trips and overwrite #parse for backbone user model.

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -1,3 +1,21 @@
 SofaHopping.Models.User = Backbone.Model.extend({
-  urlRoot: "/api/users"
+  urlRoot: "/api/users",
+
+  trips: function(){
+    if (!this._trips){
+      this._trips = new SofaHopping.Collections.Trips([], { user: this });
+    }
+
+    return this._trips;
+  },
+
+  parse: function(payload){
+    debugger
+    if (payload.trips){
+      this.trips().set(payload.trips);
+    }
+
+    delete payload.trips;
+    return payload;
+  }
 });
