0528a00fcdbf9fdaed04af6ee31990c3aa72ed51
Rafael <rafaeljmaldonado@gmail.com>
2015-07-14 22:21:48 -0400

    Style search results and navbar.

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,11 +25,12 @@ class User < ActiveRecord::Base
     user
   end
 
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
-  
+
 
   def valid_password?(password)
     BCrypt::Password.new(self.password_digest).is_password?(password)
