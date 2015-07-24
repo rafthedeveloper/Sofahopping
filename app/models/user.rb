82641a5803cf53c1607f49a077920dc6edde2db4
Rafael Maldonado <RafaelJMaldonado@gmail.com>
2015-07-24 13:51:01 -0400

    Test push.

--- app/models/user.rb
+++ app/models/user.rb
@@ -96,6 +96,7 @@ class User < ActiveRecord::Base
 
 
 
+
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user && user.valid_password?(password)
